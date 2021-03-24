9eb2b05ee472e1557eb630915ed83647811f10ef
davidvidovic <david.vidovic2000@gmail.com>
2021-03-24 15:42:09 +0100

    prepravka v3.8

--- ume/v3.8/tb_magnitudeComparator8bit.vhd
+++ ume/v3.8/tb_magnitudeComparator8bit.vhd
@@ -11,18 +11,18 @@ end tb_magnitudeComparator;
 
 architecture Behavioral of tb_magnitudeComparator is
 
-    component magnitudeComparator is
+    entity magnitudeComparator8bit is
     Port (
         a, b : in std_logic_vector(7 downto 0);
         lte, gte, neq : out std_logic
      );
-    end component magnitudeComparator;
+    end magnitudeComparator8bit;
 
     signal s_a, s_b : std_logic_vector(7 downto 0);
     signal s_lte, s_gte, s_neq : std_logic;
 
 begin
-    duv: magnitudeComparator
+    duv: magnitudeComparator8bit
     port map (
         a => s_a,
         b => s_b,
@@ -32,10 +32,10 @@ begin
         
     stimGen: process
     begin
-        s_a <= "01000110", "00011101" after 100 ns, "11110000" after 200 ns, "00001100" after 300 ns;
-        s_b <= "01100011", "11111000" after 100 ns, "11110000" after 200 ns, "11011111" after 300 ns;
-        --s_a <= X"00", X"FF" after 100 ns, X"33" after 200 ns, X"79" after 300 ns;
-        --s_b <= X"45", X"F0" after 100 ns, X"33" after 200 ns, X"79" after 300 ns; 
+        --s_a <= "01000110", "00011101" after 100 ns, "11110000" after 200 ns, "00001100" after 300 ns;
+        --s_b <= "01100011", "11111000" after 100 ns, "11110000" after 200 ns, "11011111" after 300 ns;
+        s_a <= X"00", X"FF" after 100 ns, X"33" after 200 ns, X"79" after 300 ns;
+        s_b <= X"45", X"F0" after 100 ns, X"33" after 200 ns, X"79" after 300 ns; 
         wait;
     end process;
 
