6a22b2d66e37892c7e16bb01ed0dbcb2f5875ce6
davidvidovic <david.vidovic2000@gmail.com>
2021-03-31 17:09:05 +0200

    ispravka v4.3

--- ume/V4.3/srFF.vhd
+++ ume/V4.3/srFF.vhd
@@ -6,23 +6,30 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity srFF is
     Port (
-    s, r, clk : in std_logic;
-    q : out std_logic );
+        s, r, clk : in std_logic;
+        q : out std_logic );
 end srFF;
 
 architecture Behavioral of srFF is
-
+    signal q_n, q_s : std_logic;
 begin
-    isFallingEdge: process(clk) is
+    opadajucaIvica: process(clk)
     begin
-        if (falling_edge(clk)) then
-            if(s='0' and r='1') then
-                q <= '0';
-            elsif(s='1' and r='0') then
-                q <= '1';
-            elsif(s='1' and r='1') then
-                q <= 'X';
-            end if;
+        if(falling_edge(clk)) then
+            q_s <= q_n;
         end if;
     end process;
+    
+    SRFF: process
+    begin
+        if(s='0' and r='0') then
+            q_n <= q_s;
+        elsif(s='1' and r='0') then
+            q_n <= '1'; 
+        elsif(s='0' and r='1') then
+            q_n <= '0';
+        else q_n <= 'X';  
+     end if;
+     end process;
+     q <= q_s;  
 end Behavioral;
